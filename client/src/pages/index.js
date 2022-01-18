841428d45e831ca4955a3f1bebbd9cd5dbc04ae9
Jakirul <jakirul.islam@live.co.uk>
2022-01-18 14:34:16 +0000

    Added join quiz page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as LeaderBoard } from "./LeaderBoard";
 export { default as LoginRegister } from "./LoginRegister";
 export { default as QuizPage } from "./QuizPage";
 export { default as QuizWaiting } from "./QuizWaiting";
+export { default as QuizJoin } from "./JoinQuiz"
