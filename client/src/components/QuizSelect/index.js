77f9a02efea707c5d9e1f324511a61a2234f8e5c
bethanvaughan <bpvaughan97@gmail.com>
2022-01-18 14:15:06 +0000

    api

--- client/src/components/QuizSelect/index.js
+++ client/src/components/QuizSelect/index.js
@@ -61,9 +61,9 @@ function QuizSelect() {
 
         <div>Difficulty Level</div>
         <select className="difficulty" name="difficulty">
-          <option value="Easy">Easy</option>
-          <option value="Medium">Medium</option>
-          <option value="Hard">Hard</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
         </select>
 
         <div>Number of Questions (1-20)</div>
