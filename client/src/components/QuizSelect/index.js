5f0127c4a475d482e2f40c6e88842879922d3e99
bethanvaughan <bpvaughan97@gmail.com>
2022-01-18 09:46:19 +0000

    html changes

--- client/src/components/QuizSelect/index.js
+++ client/src/components/QuizSelect/index.js
@@ -2,11 +2,25 @@ import React, { useState } from "react";
 import "./QuizSelect.css";
 import { NavBar } from "./";
 
+// fetch api trivia in here
 function QuizSelect() {
   return (
     <div className="QuizSelection">
       <h1>Select New Quiz</h1>
-      <form></form>
+      <form>
+        <div>Category</div>
+        <select>
+          <option>Volvo</option>
+          <option>new</option>
+        </select>
+
+        <div>Difficulty Level</div>
+
+        <div>Number of Questions (1-20)</div>
+        <input type="range" min="0" max="20" />
+        <div></div>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
